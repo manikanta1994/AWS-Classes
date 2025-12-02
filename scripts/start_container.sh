f96f9e9ee2ac3187e85b85715a8135f3d71c7102
manikanta1994 <33510091+manikanta1994@users.noreply.github.com>
2025-12-02 10:17:30 +0530

    Update comment to remove 'Hub' from Docker image pull

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from Docker
 docker pull manikanta1994/simple-python-app:latest
 
 # Run the Docker image as a container
